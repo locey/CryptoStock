7874a5fefca1f673acb32506f4edeebd5f5295fc
3586918451 <3586918451@qq.com>
2025-09-24 21:28:10 +0800

    同步服务

--- StockCoinSync/db/migrations/01_create.sql
+++ StockCoinSync/db/migrations/01_create.sql
@@ -20,8 +20,27 @@ create table ob_indexed_status
     chain_id           bigint  default 1 not null comment '链id (1:以太坊, 56: BSC)',
     last_indexed_block bigint  default 0 null comment '区块号',
     last_indexed_time  bigint            null comment '最后同步时间戳',
-    index_type         tinyint default 0 not null comment '0:activity, 1:trade info, 2:listing,3:sale,4:exchange,5:floor price',
+    index_type         tinyint default 0 not null comment '0:stocktoken',
     create_time        bigint            null,
     update_time        bigint            null
+)
+    collate = utf8mb4_general_ci;
+create table ob_activity_sepolia
+(
+    id                 bigint auto_increment comment '主键'
+        primary key,
+    activity_type      tinyint                 not null comment '(1:Buy,2:Sell)',
+    user_address       varchar(42)             null comment '对于buy,sell类型指的是交易的发起者，即用户address',
+    token_symbol       varchar(10)             not null,
+    current_price      decimal(30) default 0   comment '货币价格(1表示eth)',
+    token_amount       decimal(30) default 0   not null comment 'token数量',
+    currency_amount    decimal(30) default 0   not null comment '法币数量',
+    block_number       bigint      default 0   not null comment '区块号',
+    tx_hash            varchar(66)             null comment '交易事务hash',
+    event_time         bigint                  null comment '链上事件发生的时间',
+    create_time        bigint                  null comment '创建时间',
+    update_time        bigint                  null comment '更新时间',
+    constraint index_tx_stock_token_type
+        unique (tx_hash, token_symbol,  activity_type)
 )
     collate = utf8mb4_general_ci;
